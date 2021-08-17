260633c7d322eab801c38878625dfb6aae3ca6f8
fzipp <fzipp@gmx.de>
2021-08-17 22:11:21 +0200

    risc/internal/fp: no absolute paths in gen.go

--- risc/internal/fp/test/gen.go
+++ risc/internal/fp/test/gen.go
@@ -6,9 +6,9 @@ package test
 
 // The Verilog (.v) files can be downloaded at
 // http://people.inf.ethz.ch/wirth/ProjectOberon/index.html
-//go:generate go run v2go/v2go.go -o add/add_gen.go /Users/frederik/Downloads/verilog/FPAdder.v
-//go:generate go run v2go/v2go.go -o mul/mul_gen.go /Users/frederik/Downloads/verilog/FPMultiplier.v
-//go:generate go run v2go/v2go.go -o div/div_gen.go /Users/frederik/Downloads/verilog/FPDivider.v
-//go:generate go run v2go/v2go.go -o idiv/idiv_gen.go /Users/frederik/Downloads/verilog/Divider.v
+//go:generate go run v2go/v2go.go -o add/add_gen.go FPAdder.v
+//go:generate go run v2go/v2go.go -o mul/mul_gen.go FPMultiplier.v
+//go:generate go run v2go/v2go.go -o div/div_gen.go FPDivider.v
+//go:generate go run v2go/v2go.go -o idiv/idiv_gen.go Divider.v
 
 //go:generate go run numbers/numbers.go -o numbers_gen.go
